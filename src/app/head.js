49448f8cf5a5ed55ae66788094dfe146b8fe6bc3
Rodrigo Sakae <devsakae@gmail.com>
2023-02-23 17:09:03 -0300

    feat: new project! movies I liked

---
+++ src/app/head.js
@@ -0,0 +1,9 @@
+export default function Head() {
+  return (
+    <>
+      <title>Movies seen by @devsakae</title>
+      <meta description="Movies that developer Rodrigo Sakae watched and recommends" />
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+    </>
+  )
+}
